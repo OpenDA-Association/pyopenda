b0613e30c8427d11d3899608a472a58f96ed9bb6
Nils van Velzen <nilsvanvelzen@gmail.com>
2023-09-06 12:25:41 +0200

    Added and teste timeselection on stochobs

--- tests/test_pandas_observer.py
+++ tests/test_pandas_observer.py
@@ -3,6 +3,7 @@ assert pytest   #Just to make pyflake happy on a false positive for not using py
 import os
 import unittest as need_assertions
 from openda.observers.pandas_observer import PandasObserver
+from openda.costFunctions.JObjects import PyTime
 
 assertions = need_assertions.TestCase('__init__')
 
@@ -11,7 +12,7 @@ def test1():
         'store_name': None,
         'working_dir': '.',
         'config_file': 'obs.csv',
-        'labels': ['obs1', 'obs2'],
+        'labels': ['obs1'],
         'std': [0.3, 0.4]
     }
 
@@ -23,6 +24,17 @@ def test1():
     obs_descr = obs.get_observation_descriptions()
     keys = obs_descr.get_property_keys()
     ids = obs_descr.get_properties('id')
+
+    obs_now = obs.create_selection(time_span=PyTime(times[0].get_mjd(), times[1].get_mjd()))
+    vals_n = obs_now.get_values()
+    vals_noise_n = obs_now.get_realizations()
+    std_n = obs_now.get_standard_deviation()
+    times_n = obs_now.get_times()
+    obs_descr_n = obs_now.get_observation_descriptions()
+    keys_n = obs_descr_n.get_property_keys()
+    ids_n = obs_descr_n.get_properties('id')
+
+
 
 
     assert True
