9454b113e48a9e581c4f907ede429adf5e9d03f4
AronSchouten <a.a.schouten@student.tudelft.nl>
2023-11-09 14:45:40 +0100

    Added first implementation of smoother for comparison

---
+++ openda/models/SaintVenantWithSmootherFactory.py
@@ -0,0 +1,43 @@
+import numpy as np
+from openda.models.SaintVenantWithSmootherInstance import SaintVenantWithSmootherInstance
+
+
+class SaintVenantWithSmootherFactory:
+    """
+    Interface of Saint-Venant Model Factory with Smoother
+
+    @author Aron Schouten
+    """
+
+    def __init__(self, f=None):
+        """
+        Constructor
+        """
+        if f is None:
+            f = np.random.uniform(1e-4, 1e-3)
+
+        names = ["D", "f", "g", "L", "n"]
+        param_values = [15, f, 9.81, 55.e3, 11]
+        param_uncertainty = [0, 0.00001, 0, 0, 0]
+
+        param = dict(zip(names, param_values))
+        param_uncertainty = dict(zip(names, param_uncertainty))
+
+        state = np.zeros(2*param['n'] + 1)
+        state_uncertainty = np.ones(2*param['n'] + 1) * 0.02
+        state_uncertainty[-1] = 0
+
+        reftime = np.datetime64('2022-02-18T08:00:00')
+        span = [reftime, np.timedelta64(5,'m'), reftime+np.timedelta64(2,'D')] # [0 seconds, 10 minutes, 2 days]
+
+        self.model_attributes = (param, param_uncertainty, state, state_uncertainty, span)
+
+    def get_instance(self, noise_config, main_or_ens):
+        """
+        Create an instance of the stochastic Model.
+
+        :param noise_config: dictionary as given by EnkfAlgorithm.xml for the noise configuration.
+        :param main_or_ens: determines the ouput level of the model.
+        :return: the stochastic Model instance.
+        """
+        return SaintVenantWithSmootherInstance(self.model_attributes, noise_config, main_or_ens)
